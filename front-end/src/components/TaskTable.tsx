16871f0a94dd1924d18ae2e8c75abd1fa110979c
sk-phan <sukiphan97@gmail.com>
2023-09-10 13:49:37 +0300

    added name  input and event

--- front-end/src/components/TaskTable.tsx
+++ front-end/src/components/TaskTable.tsx
@@ -7,6 +7,10 @@ interface Props {
 
 const TaskTable = ({ tasks } : Props) => {
 
+    const onChangeTaskName = (name: string) => {
+
+    }
+
     return (
         <table className="custom-table">
             <thead>
@@ -19,9 +23,15 @@ const TaskTable = ({ tasks } : Props) => {
             <tbody>
                 {tasks.map((item) => (
                 <tr key={item.id}>
-                    <td>{item.name}</td>
-                    <td>{new Date(item.dueDate).toLocaleString()}</td>
-                    <td>{item.status}</td>
+                    <td className="field">
+                        <input 
+                        className="editable-input" 
+                        value={item.name} 
+                        type="text" 
+                        onChange={(e) => onChangeTaskName(e.target.value)}/>
+                    </td>
+                    <td className="field">{new Date(item.dueDate).toLocaleString()}</td>
+                    <td className="field">{item.status}</td>
                 </tr>
                 ))}
             </tbody>
