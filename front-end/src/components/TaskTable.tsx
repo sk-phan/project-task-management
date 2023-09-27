84186fb797f5c7d8ad94d74c59917a889ffb8234
sk-phan <sukiphan97@gmail.com>
2023-09-27 19:53:40 +0300

    update task inline

--- front-end/src/components/TaskTable.tsx
+++ front-end/src/components/TaskTable.tsx
@@ -1,5 +1,7 @@
 import { Task } from "../types"
 import "../styles/TaskTable.css"
+import taskService from "../utils/taskService"
+import { useEffect, useState } from "react"
 
 interface Props {
     tasks: Task[]
@@ -7,8 +9,30 @@ interface Props {
 
 const TaskTable = ({ tasks } : Props) => {
 
-    const onChangeTaskName = (name: string) => {
+    const [ taskItems, setTaskItems ] = useState<Task[]>(tasks)
 
+    useEffect(() => setTaskItems(tasks), [tasks])
+    
+    const onUpdateTask = (type: string, value: string, id: string) => {
+        const updatedTasks = taskItems.map((task) => {
+            if (task.id === id) {
+              if (type === "name") {
+                return { ...task, name: value };
+              }
+            }
+            return task;
+          });
+
+        setTaskItems(updatedTasks)
+
+        const editedTask = updatedTasks.find((task) => task.id === id);
+
+        if (editedTask) {
+
+            taskService.update(editedTask)
+            .then(res => console.log(res.data))
+            .catch(e => console.log(e))
+        }
     }
 
     return (
@@ -21,14 +45,14 @@ const TaskTable = ({ tasks } : Props) => {
                 </tr>
             </thead>
             <tbody>
-                {tasks.map((item) => (
+                {taskItems.map((item) => (
                 <tr key={item.id}>
                     <td className="field">
                         <input 
                         className="editable-input" 
                         value={item.name} 
                         type="text" 
-                        onChange={(e) => onChangeTaskName(e.target.value)}/>
+                        onChange={(e) => onUpdateTask('name', e.target.value, item.id)}/>
                     </td>
                     <td className="field">{new Date(item.dueDate).toLocaleString()}</td>
                     <td className="field">{item.status}</td>
--- front-end/src/utils/taskService.ts
+++ front-end/src/utils/taskService.ts
@@ -5,6 +5,12 @@ const taskService = {
     getAll(projectId: string) {
         return api.get<Task[]>('/tasks/' + projectId);
     },
+    update(task: Task){
+        return api.put('/tasks/update/' + task.id, task)
+    },
+    delete(taskId: string){
+        return api.put('/tasks/delete/' + taskId)
+    }
 };
 
 export default taskService;
