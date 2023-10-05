e059c93ea2689f3a5e1d2e31d5f4e3c63ee6c614
sk-phan <sukiphan97@gmail.com>
2023-10-05 19:23:14 +0300

    revert config.js file to use local and production database

--- back-end/utils/config.js
+++ back-end/utils/config.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const localMongoURI = "mongodb://localhost:27017/task-management";
 const productionMongoURI = process.env.MONGODB_URI;
 
-const MONGODB_URI =  productionMongoURI ;
+const MONGODB_URI = isProduction ? productionMongoURI : localMongoURI;
 
 module.exports = {
   MONGODB_URI,
