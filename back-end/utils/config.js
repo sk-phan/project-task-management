3ba6df727aa98fa9d52b1f0b95b2a2e2a2455baa
sk-phan <sukiphan97@gmail.com>
2023-10-05 19:08:18 +0300

    update

--- back-end/utils/config.js
+++ back-end/utils/config.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const localMongoURI = "mongodb://localhost:27017/task-management";
 const productionMongoURI = process.env.MONGODB_URI;
 
-const MONGODB_URI = isProduction ? productionMongoURI : localMongoURI;
+const MONGODB_URI =  productionMongoURI ;
 
 module.exports = {
   MONGODB_URI,
