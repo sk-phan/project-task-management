54dcaa0decde1f94ccfd46091a42cc6ee6542220
Suki Phan <suki@diffosolutions.com>
2024-01-25 10:00:31 +0200

    updated

--- back-end/utils/config.js
+++ back-end/utils/config.js
@@ -3,9 +3,10 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3000;
 const isProduction = process.env.NODE_ENV === 'production';
 const localMongoURI = "mongodb://localhost:27017/task-management";
-const productionMongoURI = process.env.MONGODB_URI;
+const productionMongoURI = "mongodb+srv://sukiphan97:Nhung1967@cluster0.syysth0.mongodb.net/?retryWrites=true&w=majority";
 
-const MONGODB_URI = isProduction ? productionMongoURI : localMongoURI;
+
+const MONGODB_URI = productionMongoURI;
 
 module.exports = {
   MONGODB_URI,
