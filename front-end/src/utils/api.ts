255cca5073f7bb7113890534dc9a3d108d7090b0
Suki Phan <suki@diffosolutions.com>
2024-01-25 09:41:57 +0200

    change url

--- front-end/src/utils/api.ts
+++ front-end/src/utils/api.ts
@@ -5,11 +5,8 @@ export const logout = () => {
   window.location.href = '/';
 };
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_BASE_URL_PROD
-    : process.env.REACT_APP_API_BASE_URL_DEV;
-
+const baseURL = "https://suki-project-management.fly.dev"
+    
 // Create a new Axios instance
 const api = axios.create({
   baseURL, // Replace with your API's base URL
