f283022c4fe79918304b61665a1fd7d8524e32d3
sk-phan <sukiphan97@gmail.com>
2024-02-01 14:50:28 +0200

    remove TestA

--- front-end/src/store/counterReducer.ts
+++ front-end/src/store/counterReducer.ts
@@ -1,17 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { Project, Task } from '../types'
+import { StateFromReducersMapObject } from 'redux'
 
 // Define a type for the slice state
 interface CounterState {
-  value: number,
   project: Project
   tasks: Task[]
 }
 
 // Define the initial state using that type
 const initialState: CounterState = {
-  value: 10,
   project: {
     name: "",
     tasks: [],
@@ -25,25 +24,16 @@ export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
-    },
-    setTasks: (state, action: PayloadAction<Task[]>) => {
+    setTasks: (state: CounterState, action: PayloadAction<Task[]>) => {
       state.tasks = [...action.payload]
     },
-    setProject: (state, action: PayloadAction<Project>) => {
+    setProject: (state: CounterState, action: PayloadAction<Project>) => {
       state.project = {...action.payload}
     },
   },
 })
 
-export const { increment, decrement, incrementByAmount, setTasks, setProject } = counterSlice.actions
+export const { setTasks, setProject } = counterSlice.actions
 
 
 export default counterSlice.reducer
